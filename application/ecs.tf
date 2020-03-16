d8fed7c7af0e4f7d623f9aa7daad8704a13e9737
Lloyd Parkes <lloyd@must-have-coffee.gen.nz>
2020-03-16 15:13:54 +1300

    Fixup the ingress and egress rules for the security groups.

--- application/ecs.tf
+++ application/ecs.tf
@@ -89,6 +89,15 @@ resource "aws_security_group" "app_tasks" {
     from_port       = var.container.port
     to_port         = var.container.port
     protocol        = "tcp"
+    security_groups = [ aws_security_group.alb.id ]
+  }
+
+  egress {
+    description = "Access to ECR"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -186,6 +195,15 @@ resource "aws_security_group" "alb" {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Access to the application"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = [ var.cidr_block ]
   }
 }
 
